e87086619ae3613d9629067b40027e4ada32b1c1
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2023-10-27 12:01:09 +0300

    düzeltmeler

--- armstrongNumber.dart
+++ armstrongNumber.dart
@@ -1,27 +1,38 @@
+
 import 'dart:io';
 import 'dart:math';
+void main(){
+  
+  int? num;
+  print('Kontrol etmek istediğiniz sayıyı buraya giriniz:');
+  num = int.parse(stdin.readLineSync()!);//kullanıcıdan veri almak
+ 
+  isArmString(num);
 
-bool isArmstrongNumber(int number) {
-  int originalNumber = number;
-  int numberOfDigits = number.toString().length;
-  int sum = 0;
-
-  while (number > 0) {
-    int digit = number % 10;
-    sum += pow(digit, numberOfDigits).toInt();
-    number ~/= 10;
-  }
-
-  return sum == originalNumber;
 }
 
-void main() {
-  print("Kontrol etmek istediğiniz sayıyı buraya giriniz: ");
-  int number = int.parse(stdin.readLineSync()!);
+void isArmString(int number){
+  
+  int temp,digits =0, last = 0, sum = 0;//değişkenler tanımlanır.
 
-  if (isArmstrongNumber(number)) {
-    print('$number bir Armstrong sayısıdır.');
-  } else {
-    print('$number bir Armstrong sayısı değildir.');
+  temp = number;
+
+  while(temp>0){//0'dan büyüklük şartı ile döngü başlar.
+    temp = temp~/10;//basamak sayısını kontrol etmek için 10'a bölünür.
+    digits++;
+  }
+
+  temp = number;
+
+  while(temp>0){
+    last = temp % 10;
+    sum = sum + pow(last, digits) as int;
+    temp = temp~/10;   
+  }
+
+  if(number == sum){
+    print("$number bir Armstrong sayısıdır.");
+  }else{
+    print("$number bir Armstrong sayısı değildir.");
   }
 }
